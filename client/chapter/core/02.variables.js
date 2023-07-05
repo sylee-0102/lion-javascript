da90e0d21235d3279c4b076c8fc926744b486d56
sylee-0102 <design_sylee@naver.com>
2023-07-05 09:18:28 +0900

    add. 2일차 수업 - README.md 추가 - 변수와 상수에 대한 이해 - 엄격 모드란? - 전역 객체를 선택하는 방법

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,80 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+
+
+let admin;
+let nickname;
+
+// 참조의 의한 복사
+
+nickname = 'suyeon';
+
+admin = nickname;
+
+console.log(admin);
+
+
+const ourPanetName = 'Earth';
+const currentOnlineUserName = 'suyeon';
+
+
+
+
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasClassName = false;
+
+// - 구매 결제 내역
+let paymentHistory;
+
+// - 브랜드 접두사
+const brand_prefix = 'NIKE';
+
+// - 오늘의 운세
+let fortuneOfToday;
+
+// - 상품 정보
+const productInformation = '';
+
+
+
+
+/* variables ----------------------------------------------------------- */
+
+// - 갯수 별 상품 가격 계산하기 > let = sangPoomGyeSan
+// - 구매 제품 가격의 총 합 > let = totlaPrice
+// - 구매 결제 여부 > let = moneyYesOrNo
+// - 구매 결제 내역 > let = listOfBuy
+// - 오늘의 운세 > let = toDayUnSay
+
+
+/* constant variables -------------------------------------------------- */
+
+// - 브랜드 접두사 > const = brandPrefix
+// - 상품 정보 >  const = information
+
+// - 1년 기준 일(day)자 수 > const = DATEOFYEAR
+
+
+
+
+
+
+
+
+
